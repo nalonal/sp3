1483cc2bb85719614d7f7f27822342d119016685
Leonardional <leonardional@gmail.com>
2022-08-07 19:57:12 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+## INSTALASI
+git clone https://github.com/nalonal/project3<br>
+cd project3<br>
+chmod u+x install.sh<br>
+./install.sh
+
+## PEMAKAIAN SETIAP HABIS STOP ENGINE
+sudo chmod 666 /var/run/docker.sock
---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+sudo apt update
+sudo apt install lsb-release ca-certificates apt-transport-https software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+sudo apt install docker-ce
+mkdir -p ~/.docker/cli-plugins/
+curl -SL https://github.com/docker/compose/releases/download/v2.5.0/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
+chmod +x ~/.docker/cli-plugins/docker-compose
+sudo chmod 666 /var/run/docker.sock
+docker compose up -d
+sudo apt install python3-pip
+pip3 install kafka-python
